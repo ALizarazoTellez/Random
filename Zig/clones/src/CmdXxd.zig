af56a8f2c60b87742092391953ec843ab8452ddf
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2025-01-26 16:18:02 -0500

    Show file size

--- Zig/clones/src/CmdXxd.zig
+++ Zig/clones/src/CmdXxd.zig
@@ -1,7 +1,22 @@
 const std = @import("std");
 
 pub fn run(args: []const []const u8) void {
-    for (args) |arg| {
-        std.debug.print("Arg: {s}\n", .{arg});
-    }
+    const stdout = std.io.getStdOut().writer();
+    const filename = args[args.len - 1];
+
+    const file = std.fs.cwd().openFile(filename, .{ .mode = std.fs.File.OpenMode.read_only }) catch |err| {
+        stdout.print("Unexpected error: {}\n", .{err}) catch {};
+        return;
+    };
+    defer file.close();
+
+    const metadata = file.metadata() catch |err| {
+        stdout.print("Unexpected error: {}\n", .{err}) catch {};
+        return;
+    };
+
+    std.debug.print(
+        \\Path: {s}
+        \\Size: {}
+    ++ "\n", .{ filename, metadata.size() });
 }
