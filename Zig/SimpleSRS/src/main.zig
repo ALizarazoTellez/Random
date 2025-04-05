a72c2093c0f857b9ad216644a26280f0fefd28bb
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2025-04-05 17:13:50 -0500

    wip

--- Zig/SimpleSRS/src/main.zig
+++ Zig/SimpleSRS/src/main.zig
@@ -4,6 +4,8 @@ const std = @import("std");
 
 const zdt = @import("zdt");
 
+const srs = @import("srs.zig");
+
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
@@ -15,5 +17,10 @@ pub fn main() !void {
     var currentTz = try zdt.Timezone.fromTzdata("America/Bogota", allocator);
     defer currentTz.deinit();
 
+    const day = try srs.Day.today(&currentTz);
+
+    const fmt = try day.formated(allocator);
+
     try stdout.print("Hello {any}\n", .{zdt.Datetime.now(.{ .tz = &currentTz })});
+    try stdout.print("{s}", .{fmt});
 }
